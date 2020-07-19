de816fb050d1194f762a93fe09fb2bcd40e811fc
kiyeon <kyeoneee@gmail.com>
2020-07-19 23:42:19 +0900

    Age of Vue.js - 섹션 11. 싱글 파일 컴포넌트

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
+  <!-- <div id="app"> -->
+    <!-- 파스칼 케이스 -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- 케밥 케이스 -->
+    <!-- <hello-world></hello-world> -->
+  <!-- </div> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
 export default {
+  // 인스턴스 옵션 속성 or 컴포턴트 옵션 속성
   name: 'App',
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
+    // HelloWorld // == 'hello-world': HelloWorld
+  },
+  methods: {
+    renewStr: function() {
+        this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
